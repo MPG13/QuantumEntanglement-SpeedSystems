14fae5ba63a8669f93cc0e00e714d4d334972cee
MPG13 (Micah Gomez) <micahplacido@gmail.com>
2018-04-03 13:41:32 -0600

    not needed

--- QuantumEntangler.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:loopback
-echo Behold, access to the firewall (via OreoKing21)
-rem please don't remove this
-chdir
-set /P theuserinput=""
-%theuserinput%
-goto loopback
-Netsh Advfirewall show allprofiles
